0ec1e9fa931c8c33027524311839b689217b6624
Rodrigo Santone <rsantone@santandertecnologia.com.ar>
2020-02-03 15:15:44 -0300

    new token

--- config/enums.py
+++ config/enums.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class Data(Enum):
     PageId = "104084117828825"
-    Token = "EAALaQsiEMcEBACcXY5APa4XIqAhi7VTO3ji2fKHkHTFZBlxn6FPZBn5T1FNw6WGaaoPQxitsK0mv1lH04MfTkc6s2XviVQpcc0mm7NcmupbQefLuo0fGE0vvPjrA83CjNxdINjyg38UDns74ZCG8OiHkO6EnDKrl3GJeD7CCHnrvoaRwZBCU7vNzZABNZBRiMs4GBWQbuKjDGHZBFHCnOzgsMdKGxcgqUrgp6lgZAM606wZDZD"
+    Token = "EAALaQsiEMcEBAL1siEIiwRlipZCBT9EnAXiLJaCknqqrSNjJwdY3DBCQpBGuHjM4aI3bzfl2KJjJ15VtcbDFjQ751YyCF6KuLrRyIdu6e50AhL31LaOUKFZCquOwWSBZACZAvqs8XnX11AfJaAaZAPGiENucjXdEZD"
 
 class Actions(Enum):
     PostMsg= "Post Message"
