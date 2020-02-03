4b16e707d847e4da986847386dbd3b8492f08310
Rodrigo Santone <rsantone@santandertecnologia.com.ar>
2020-02-03 17:13:34 -0300

    refactor variable latest post

--- tests/Test.py
+++ tests/Test.py
@@ -1,24 +1,22 @@
 from config.enums import *
 from config.tools import Tools
 
-
-class Test:
+class Test():
     api = Tools.get_api()
+    latest_post = None
+
 
     def test_create_post(self):
         self.api.put_object("me", 'feed', message=Actions.PostMsg.value)
+        Test.latest_post = Tools.get_latest_post(Test.api)
 
     def test_like_post(self):
-        latest_post = Tools.get_latest_post(self.api)
-        self.api.put_like(latest_post)
+        self.api.put_like(Test.latest_post)
 
     def test_add_comment(self):
-        latest_post = Tools.get_latest_post(self.api)
-        self.api.put_comment(latest_post,message=Actions.CommentMsg.value)
+        self.api.put_comment(Test.latest_post,message=Actions.CommentMsg.value)
     def test_update_post(self):
-        latest_post = Tools.get_latest_post(self.api)
-        self.api.request(latest_post + '?message=' + Actions.UpdateMsg.value, method='POST')
+        self.api.request(Test.latest_post + '?message=' + Actions.UpdateMsg.value, method='POST')
 
     def test_delete_post(self):
-        latest_post = Tools.get_latest_post(self.api)
-        self.api.delete_object(id=latest_post)
+        self.api.delete_object(id=Test.latest_post)
